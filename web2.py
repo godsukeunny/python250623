e3f5318d228d7ab69e1291b6daff29d07e85862d
godsukeunny <godsukeunny@gamil.com>
2025-06-25 14:21:18 +0900

    크롤링 파일에 저장

--- web2.py
+++ web2.py
@@ -5,14 +5,31 @@ from bs4 import BeautifulSoup
 #웹서버에 요청
 import urllib.request
 
-url = "https://www.clien.net/service/board/sold"
-data = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(data, "html.parser")
-list = soup.find_all("span", attrs={"data-role": "list-title-text"})
-for tag in list:
-    title = tag.text.strip()  # 태그의 텍스트 내용 추출
-    title = title.replace("\n", " ")  # 줄바꿈 제거
-    print(title)  # 태그의 텍스트 내용 출력
+f = open("clien.txt", "wt", encoding="utf-8")
+
+# url = "https://www.clien.net/service/board/sold"
+for i in range(0, 10):
+    url = f"https://www.clien.net/service/board/sold?&od=T31&category=0&po={i}"
+    print(url)
+    data = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(data, "html.parser")
+    list = soup.find_all("span", attrs={"data-role": "list-title-text"})
+    for tag in list:
+        title = tag.text.strip()  # 태그의 텍스트 내용 추출
+        print(title)  # 태그의 텍스트 내용 출력
+        f.write(title + "\n")
+
+f.close()            
+
+# url = "https://www.clien.net/service/board/sold?&od=T31&category=0&po=0"
+
+# data = urllib.request.urlopen(url).read()
+# soup = BeautifulSoup(data, "html.parser")
+# list = soup.find_all("span", attrs={"data-role": "list-title-text"})
+# for tag in list:
+#     title = tag.text.strip()  # 태그의 텍스트 내용 추출
+#     title = title.replace("\n", " ")  # 줄바꿈 제거
+#     print(title)  # 태그의 텍스트 내용 출력
 
 # <span class="subject_fixed" data-role="list-title-text" title="[예약중] 아이패드 미니 7 / 애플 케어 플러스 / 애플펜슬 프로">
 #       [예약중] 아이패드 미니 7 / 애플 케어 플러스 / 애플펜슬 프로
